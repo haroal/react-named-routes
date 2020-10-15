9c9efb5b275a0bf1a600403a3f79e8171023fbae
Alexis ANNEIX <alexis.anneix@gmail.com>
2020-10-15 21:14:11 +0200

    Updates

---
+++ precache-manifest.c2471836c94d31b9ac59cb16d1b0b67b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a401d633ab20ac411b84ce78701efc7",
+    "url": "/react-named-routes/index.html"
+  },
+  {
+    "revision": "df051fc1877ff1ece540",
+    "url": "/react-named-routes/static/js/2.9d557a00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-named-routes/static/js/2.9d557a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7191bb94128ded58cf92",
+    "url": "/react-named-routes/static/js/main.2242647d.chunk.js"
+  },
+  {
+    "revision": "42c9e2b09d34f4418dcd",
+    "url": "/react-named-routes/static/js/runtime-main.1c153390.js"
+  }
+]);
